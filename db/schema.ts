920b2c7f938961b57d484f3a159d81984c8688bc
Jaydeep Suthar <technicaljade@gmail.com>
2025-05-31 09:45:57 +0530

    refactor: rename coins to credits in db and set default value to 5

--- db/schema.ts
+++ db/schema.ts
@@ -24,7 +24,7 @@ export const users = pgTable("users", {
     deviceType: varchar("device_type", { length: 50 }),
     deviceId: varchar("device_id", { length: 255 }).unique(),
     fcmToken: varchar("fcm_token", { length: 255 }),
-    coin: integer().default(3).notNull(),
+    credits: integer().default(5).notNull(),
     noOfAdsWatch: integer("no_of_ads_watch").default(0).notNull(),
     isDeleted: boolean("is_deleted").default(false),
     ...timestamps,
