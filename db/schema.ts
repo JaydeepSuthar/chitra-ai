048396cde55712ae2a3331c106bedc3268c420b8
Jaydeep Suthar <technicaljade@gmail.com>
2025-06-06 20:37:15 +0530

    minor: added token in user schema

--- db/schema.ts
+++ db/schema.ts
@@ -27,6 +27,7 @@ export const users = pgTable("users", {
     credits: integer().default(5).notNull(),
     noOfAdsWatch: integer("no_of_ads_watch").default(0).notNull(),
     isDeleted: boolean("is_deleted").default(false),
+    token: varchar("token", { length: 255 }),
     ...timestamps,
 });
 
